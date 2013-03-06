015582e47b17379e9e1cacc0813a6960b0fa5537
RicSwirrl <ric@swirrl.com>
2013-03-06 09:30:15 +0000

    rudimentary concept scheme and ontology views

--- app/models/publish_my_data/concept_scheme.rb
+++ app/models/publish_my_data/concept_scheme.rb
@@ -4,5 +4,12 @@ module PublishMyData
     include PublishMyData::ResourceModule #some common methods for resources.
 
     rdf_type RDF::SKOS.ConceptScheme
+    field :label, RDF::RDFS.label
+
+    def concepts
+      Resource.find_by_sparql(
+        "SELECT DISTINCT ?uri ?p ?o WHERE { <#{self.uri}> <http://www.w3.org/2004/02/skos/core#hasTopConcept> ?uri }"
+      )
+    end
   end
 end
--- app/models/publish_my_data/ontology.rb
+++ app/models/publish_my_data/ontology.rb
@@ -4,5 +4,33 @@ module PublishMyData
     include PublishMyData::ResourceModule
 
     rdf_type RDF::OWL.Ontology
+    field :label, RDF::RDFS.label
+
+    def ontology_classes
+      Resource.find_by_sparql("
+        SELECT DISTINCT ?uri
+        WHERE {
+          {
+            ?uri <http://www.w3.org/2000/01/rdf-schema#isDefinedBy> <#{self.uri}> .
+            ?uri a <http://www.w3.org/2002/07/owl#Class> .
+          }
+          UNION
+          {
+            ?uri <http://www.w3.org/2000/01/rdf-schema#isDefinedBy> <#{self.uri}> .
+            ?uri a <http://www.w3.org/2000/01/rdf-schema#Class>
+          }
+        }"
+      )
+    end
+
+    def ontology_properties
+      Resource.find_by_sparql("
+        SELECT DISTINCT ?res ?p ?o
+        WHERE {
+          ?uri <http://www.w3.org/2000/01/rdf-schema#isDefinedBy> <#{self.uri}> .
+          ?uri a <http://www.w3.org/1999/02/22-rdf-syntax-ns#Property> .
+        }"
+      )
+    end
   end
 end
