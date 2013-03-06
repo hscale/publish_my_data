450e62396ee9993904b1e6d4ada4653cfcba6192
RicSwirrl <ric@swirrl.com>
2013-03-06 09:08:46 +0000

    changed dataset vocabulary

--- config/initializers/vocabularies.rb
+++ config/initializers/vocabularies.rb
@@ -1,2 +1,2 @@
 SITE_VOCAB = RDF::Vocabulary.new("http://#{PublishMyData.local_domain}/def")
-PMD_DS_VOCAB = RDF::Vocabulary.new("http://publishmydata.com/def/dataset#")
+PMD_DS_VOCAB = RDF::Vocabulary.new("http://#{PublishMyData.local_domain}/def/dataset#")
