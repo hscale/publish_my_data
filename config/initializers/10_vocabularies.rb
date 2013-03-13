53c9e99a99b4888e1da5dabfc0131795cf4196a0
RicSwirrl <ric@swirrl.com>
2013-03-13 17:23:45 +0000

    change PMD_DS vocab

--- config/initializers/10_vocabularies.rb
+++ config/initializers/10_vocabularies.rb
@@ -1,6 +1,6 @@
 module RDF
   SITE = RDF::Vocabulary.new("http://#{PublishMyData.local_domain}/def/")
-  PMD_DS = RDF::Vocabulary.new("http://#{PublishMyData.local_domain}/def/dataset#")
+  PMD_DS = RDF::Vocabulary.new("http://publishmydata.com/def/dataset#")
   DCAT = RDF::Vocabulary.new("http://www.w3.org/ns/dcat#")
   VOID = RDF::Vocabulary.new("http://rdfs.org/ns/void#")
 end
