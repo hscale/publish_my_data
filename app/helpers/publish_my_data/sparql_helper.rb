ea75b02b12e68aa354e3367d9b5c24f5c385ac99
RicSwirrl <ric@swirrl.com>
2013-03-14 11:17:31 +0000

    sparql results formats (pagination)

--- app/helpers/publish_my_data/sparql_helper.rb
+++ app/helpers/publish_my_data/sparql_helper.rb
@@ -1,7 +1,9 @@
 module PublishMyData
   module SparqlHelper
-    def link_to_sparql_results_format(text, format, query)
-      link_to text, :format => format, :query => query, :page => @page, :per_page => @per_page
+    def link_to_sparql_results_format(text, format, query, pagination_params)
+      link_params = {:format => format, :query => query}
+      link_params.merge!(:page => pagination_params.page, :per_page => pagination_params.per_page) if pagination_params
+      link_to text, link_params
     end
   end
 end
