e162184d2604717cb7c32f6c4a3e898598e72012
RicSwirrl <ric@swirrl.com>
2013-03-14 16:28:28 +0000

    fix sparql link helper

--- app/helpers/publish_my_data/sparql_helper.rb
+++ app/helpers/publish_my_data/sparql_helper.rb
@@ -1,6 +1,6 @@
 module PublishMyData
   module SparqlHelper
-    def link_to_sparql_results_format(text, format, query, pagination_params)
+    def link_to_sparql_results_format(text, format, query, pagination_params=nil)
       link_params = {:format => format, :query => query}
       link_params.merge!(:page => pagination_params.page, :per_page => pagination_params.per_page) if pagination_params
       link_to text, link_params
