f7a96810f7bc7a73b3bc67778b341c74939884d9
RicSwirrl <ric@swirrl.com>
2013-03-29 21:20:37 +0000

    logger for tripod

--- config/initializers/00_tripod.rb
+++ config/initializers/00_tripod.rb
@@ -1,7 +1,7 @@
-# set up tripod for dev mode.
 Tripod.configure do |config|
   config.query_endpoint = PublishMyData.sparql_endpoint
   config.cache_store = PublishMyData.tripod_cache_store
   config.response_limit_bytes = PublishMyData.response_limit_bytes
   config.timeout_seconds = PublishMyData.sparql_timeout_seconds
+  config.logger = Rails.logger
 end
