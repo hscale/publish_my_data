8d2a9dc001d04cca7f41183a7f14fc0ff8c5b82b
Asa Calow <mothership@asacalow.co.uk>
2013-07-17 17:06:04 +0100

    Removed redundant controllers & specs.

--- spec/controllers/publish_my_data/concept_schemes_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-module PublishMyData
-  describe ConceptSchemesController do
-    it_should_behave_like 'a controller with a dump action' do
-      let(:resource) { FactoryGirl.create(:concept_scheme) }
-    end
-  end
-end
--- spec/controllers/publish_my_data/ontologies_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-module PublishMyData
-  describe OntologiesController do
-    it_should_behave_like 'a controller with a dump action' do
-      let(:resource) { FactoryGirl.create(:ontology) }
-    end
-  end
-end
