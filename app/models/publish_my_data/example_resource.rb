941fa3aac8154d212be7cdfd6be9a215718657d7
RicSwirrl <ric@swirrl.com>
2013-08-09 14:58:50 +0100

    removed redundant types method for example res

--- app/models/publish_my_data/example_resource.rb
+++ app/models/publish_my_data/example_resource.rb
@@ -10,10 +10,6 @@ module PublishMyData
       eager_load_object_triples!(labels_only: true)
     end
 
-    def rdf_type_as_resource
-      get_related_resource(self.rdf_type, Resource)
-    end
-
     def as_ttl
       graph = RDF::Graph.new
       repository.query( [@uri, :predicate, :object] ) do |statement|
