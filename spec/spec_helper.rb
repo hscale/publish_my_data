d0cc9461893dc366f511884b3ac61442b3505122
Ash Moran <ash.moran@patchspace.co.uk>
2013-09-12 12:05:30 +0100

    Load AwesomePrint in RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ require 'rspec/autorun'
 require 'factory_girl_rails'
 require 'capybara/rails'
 require 'active_support/core_ext/numeric/bytes'
+require 'ap'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
