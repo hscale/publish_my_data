060517d809da5493e70c2584d915914847a5158e
Ash Moran <ash.moran@patchspace.co.uk>
2013-09-25 17:15:06 +0100

    Alias (backwards) the :my_dataset factory as :dataset, and allow creation based on a slug

--- spec/factories/dataset_factories.rb
+++ spec/factories/dataset_factories.rb
@@ -1,13 +1,16 @@
 FactoryGirl.define do
-  factory :my_dataset, class: PublishMyData::Dataset do
+  factory :dataset, aliases: [:my_dataset], class: PublishMyData::Dataset do
     initialize_with { new(uri, graph_uri) }
     title 'My Dataset'
     description 'A test dataset'
     modified DateTime.parse('2010-07-01 12:00')
-    data_graph_uri PublishMyData::Dataset.data_graph_uri("my/dataset")
     ignore do
-      uri { PublishMyData::Dataset.uri_from_slug("my/dataset")  }
-      graph_uri { PublishMyData::Dataset.metadata_graph_uri("my/dataset") }
+      slug "my/dataset"
+    end
+    data_graph_uri { PublishMyData::Dataset.data_graph_uri(slug) }
+    ignore do
+      uri { PublishMyData::Dataset.uri_from_slug(slug)  }
+      graph_uri { PublishMyData::Dataset.metadata_graph_uri(slug) }
     end
 
     factory :data_cube do
