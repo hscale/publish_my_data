4ace13d0730bc4f1bfb2ac47b71643596e456da9
RicSwirrl <ric@swirrl.com>
2013-10-21 11:58:30 +0100

    fall back to ThirdParty resource graph uri if data_graph_uri not set

--- app/models/publish_my_data/third_party/ontology.rb
+++ app/models/publish_my_data/third_party/ontology.rb
@@ -3,6 +3,11 @@ module PublishMyData
     class Ontology < PublishMyData::Ontology
       delegate :comment, :description, :license, :publisher, :contact_email, to: :data_resource
 
+      # if there's no data graph set, return the graph uri
+      def data_graph_uri
+        super || self.graph_uri
+      end
+
       # Overrides
       ['to_rdf', 'to_ttl', 'to_nt', 'to_json'].each do |method_name|
         define_method method_name do |opts={}|
