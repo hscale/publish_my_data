7d0c8f34f4e836047d71db7f7c9817321864644d
Asa Calow <asacalow@gmail.com>
2013-12-11 15:09:21 +0000

    Dataset atom feeds.

--- app/controllers/publish_my_data/information_resources_controller.rb
+++ app/controllers/publish_my_data/information_resources_controller.rb
@@ -11,15 +11,20 @@ module PublishMyData
     # http://example.com/data/blah
     def data
       uri = "http://#{PublishMyData.local_domain}/data/#{params[:id]}"
-      resource = PublishMyData::Resource.find(uri, local: true)
-      render_resource(resource)
+      @resource = PublishMyData::Resource.find(uri, local: true)
+      respond_with(@resource) do |format|
+        format.html { render_resource(@resource) }
+        format.atom { @resource.is_a?(PublishMyData::Dataset) ? render(template: template_for_resource(@resource)) : head(406) }
+      end
     end
 
     # http://example.com/def/blah
     def def
       uri = "http://#{PublishMyData.local_domain}/def/#{params[:id]}"
       resource = PublishMyData::Resource.find(uri, local: true)
-      render_resource(resource)
+      respond_with(resource) do |format|
+        format.html { render_resource(resource) }
+      end
     end
 
     # http://example.com/def/blah/dump
